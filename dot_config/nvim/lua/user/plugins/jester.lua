73dd7029a19949e11225fbddaf275237e96f0ab9
nfort <laikisounds@gmail.com>
2023-08-23 10:03:19 +0300

    Remove .config/nvim/lua/user/plugins/jester.lua

--- dot_config/nvim/lua/user/plugins/jester.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "David-Kunz/jester",
-    event = "BufRead",
-    config = function()
-      require("jester").setup({ cmd = "npx jest -t '$result' --detectOpenHandles --collectCoverage=false -- $file" })
-    end,
-  },
-}
