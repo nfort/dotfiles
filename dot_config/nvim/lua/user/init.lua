42f662c6f78c015dfd520db43b7faec98a4119d6
nfort <laikisounds@gmail.com>
2023-08-25 10:22:52 +0300

    Update .config/nvim/lua/user/init.lua

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -2,8 +2,8 @@ return {
   options = {
     opt = {
       colorcolumn = "120",
-    }
-  }
+    },
+  },
   lsp = {
     formatting = {
       timeout_ms = 5000, -- adjust the timeout_ms variable for formatting
