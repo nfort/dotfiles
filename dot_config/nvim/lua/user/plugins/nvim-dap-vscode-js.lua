dcdc9bd500b0e835f508134c3b2d7c5b04598842
nfort <laikisounds@gmail.com>
2023-09-11 19:15:10 +0300

    Add .config/nvim/lua/user/plugins/neotest.lua Add .config/nvim/lua/user/plugins/nvim-dap-vscode-js.lua

---
+++ dot_config/nvim/lua/user/plugins/nvim-dap-vscode-js.lua
@@ -0,0 +1,60 @@
+local utils = require "astronvim.utils"
+
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = { "mxsdev/nvim-dap-vscode-js" },
+  optional = true,
+  config = function()
+    local dap = require "dap"
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "127.0.0.1",
+      port = "${port}",
+      executable = {
+        command = "node",
+        args = {
+          require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+            .. "/js-debug/src/dapDebugServer.js",
+          "${port}",
+        },
+      },
+    }
+    local js_config = {
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+      },
+      {
+        type = "pwa-node",
+        request = "launch",
+        name = "Debug Jest Tests",
+        -- trace = true, -- include debugger info
+        runtimeExecutable = "node",
+        runtimeArgs = {
+          "./node_modules/jest/bin/jest.js",
+          "--runInBand",
+        },
+        rootPath = "${workspaceFolder}",
+        cwd = "${workspaceFolder}",
+        console = "integratedTerminal",
+        internalConsoleOptions = "neverOpen",
+      },
+      {
+        type = "pwa-node",
+        request = "attach",
+        name = "Attach",
+        processId = require("dap.utils").pick_process,
+        cwd = "${workspaceFolder}",
+      },
+    }
+
+    if not dap.configurations.javascript then
+      dap.configurations.javascript = js_config
+    else
+      utils.extend_tbl(dap.configurations.javascript, js_config)
+    end
+  end,
+}
