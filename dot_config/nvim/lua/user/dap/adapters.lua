408d9f67340073f67574e3c30b2d9241face2e89
nfort <laikisounds@gmail.com>
2023-09-11 19:14:07 +0300

    Add .config/nvim/lua/user/dap/adapters.lua Add .config/nvim/lua/user/dap/configurations.lua

---
+++ dot_config/nvim/lua/user/dap/adapters.lua
@@ -0,0 +1,16 @@
+return {
+  'pwa-node' = {
+      type = "server",
+      host = "l2calhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        -- 💀 Make sure to update this path to point to your installation
+        args = {
+          require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+            .. "/js-debug/src/dapDebugServer.js",
+          "${port}",
+        },
+      },
+  }
+}
---
+++ dot_config/nvim/lua/user/dap/configurations.lua
@@ -0,0 +1,19 @@
+return {
+  javascript = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Debug Jest Tests",
+      -- trace = true, -- include debugger info
+      runtimeExecutable = "node",
+      runtimeArgs = {
+        "./node_modules/jest/bin/jest.js",
+        "--runInBand",
+      },
+      rootPath = "${workspaceFolder}",
+      cwd = "${workspaceFolder}",
+      console = "integratedTerminal",
+      internalConsoleOptions = "neverOpen",
+    }
+  }
+}
