9e448ed5c627676a1554c15b12af2d19ed5f9f4e
nfort <laikisounds@gmail.com>
2023-12-11 21:43:00 +0300

    Update .config/nvim/lua/user/plugins/nvim-dap-vscode-js.lua

--- dot_config/nvim/lua/user/plugins/nvim-dap-vscode-js.lua
+++ dot_config/nvim/lua/user/plugins/nvim-dap-vscode-js.lua
@@ -31,7 +31,7 @@ return {
         type = "pwa-node",
         request = "launch",
         name = "Debug Jest Tests",
-        -- trace = true, -- include debugger info
+        trace = true, -- include debugger info
         runtimeExecutable = "node",
         runtimeArgs = {
           "./node_modules/jest/bin/jest.js",
