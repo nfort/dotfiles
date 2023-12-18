dca88a88c0ed0a5a1f2f673cdc4743a8a33fba39
nfort <laikisounds@gmail.com>
2023-12-18 13:20:27 +0300

    Update .config/nvim/lua/user/init.lua

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -1,6 +1,10 @@
 vim.cmd [[
   source /home/nfort/.config/nvim/lua/user/syntax/nginx.vim
-  au BufRead,BufNewFile /etc/nginx/*,/etc/nginx/conf.d/*,/usr/local/nginx/conf/* if &ft == '' | setfiletype nginx | endif
+  " au BufRead,BufNewFile /etc/nginx/*,/etc/nginx/conf.d/*,/usr/local/nginx/conf/* if &ft == '' | setfiletype nginx | endif
+  au BufRead,BufNewFile *.nginx set ft=nginx
+  au BufRead,BufNewFile */etc/nginx/* set ft=nginx
+  au BufRead,BufNewFile */usr/local/nginx/conf/* set ft=nginx
+  au BufRead,BufNewFile nginx.conf set ft=nginx
 ]]
 
 return {
