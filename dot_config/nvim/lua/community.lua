a8a7c909947fe2058f15e23d4be12dc2d81a6070
nfort <laikisounds@gmail.com>
2024-06-01 17:56:49 +0300

    Update .config/nvim/lua/community.lua

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -4,13 +4,15 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.editing-support.multicursors-nvim" },
-  { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.media.vim-wakatime" },
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.html-css" },
+	{ import = "astrocommunity.pack.tailwindcss" },
+	{ import = "astrocommunity.pack.typescript" },
+	{ import = "astrocommunity.pack.go" },
+	{ import = "astrocommunity.test.neotest" },
+	{ import = "astrocommunity.editing-support.multicursors-nvim" },
+	{ import = "astrocommunity.motion.nvim-surround" },
+	{ import = "astrocommunity.media.vim-wakatime" },
+	-- import/override with your plugins folder
 }
